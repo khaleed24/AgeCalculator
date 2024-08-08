fc9ea6d827db5d131e483f7f182a3d743229e8c4
khaleed24 <mailbooth.id@gmail.com>
2024-08-08 23:59:33 +0100

    addressed issues

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
 document.getElementById('age-calc').addEventListener('submit',function(event) {
     event.preventDefault();
-    const birthYear = document.getElementById('birthYear').value;
-    const currentYear = new Date().getFullYear();
-    const age = currentYear - birthYear;
+
+    const birthDate = new Date(document.getElementById('birthYear').value);
+    const currentDate = new Date();
+
+    if(isNaN(birthDate.getTime()) || birthDate > currentDate) {
+        document.getElementById('result').textContent = 'Please enter a valid Date of Birth.';
+        return;
+    }
+
+    let differenceInMilliseconds = currentDate - birthDate
+
+    const seconds = Math.floor(differenceInMilliseconds / 1000);
+    differenceInMilliseconds -= seconds * 1000;
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+
+    const days = Math.floor (hours / 24);
+    const remainingHours = hours % 24;
+
+    const weeks = Math.floor (days / 7);
+    const remainingDays = days % 7;
+
+    const years = currentDate.getFullYear() - birthDate.getFullYear();
+    let months = currentDate.getMonth() - birthDate.getMonth();
+
+    if (months < 0) {
+        months += 12;
+    }
     
+    if (currentDate.getDate() < birthDate.getDate() ) {
+        months--;
+        if ( months < 0) {
+            months += 12;
+            years--;
+        }
+    }
     const calculate = document.getElementById('result');
 
-    if (birthYear > currentYear || birthYear < 1900 || birthYear ==="") {
-       calculate.textContent ='Please enter a valid year of birth.';
-
-    } else {
-        calculate.textContent = `You are ${age} years old.`;
-    }
+        calculate.textContent = `You are ${years} years, ${months} months, ${weeks} weeks, ${remainingDays} days,` + 
+                                `${remainingHours} hours, ${remainingMinutes} minutes, and ${remainingSeconds} seconds.` ;
 });
